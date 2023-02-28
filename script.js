fdab0d7622c17c98287895634b55f84c54f5dc4b
ManishaLalage <manishalalage91@gmail.com>
2023-02-28 10:29:02 -0600

    New Arrival Section Added

--- script.js
+++ script.js
@@ -9,3 +9,26 @@
 
 
 
+  var swiper = new Swiper(".arrivals-slider", {
+    loop: true,
+    grabCursor : true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      550: {
+        slidesPerView: 2,
+      },
+      850: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
